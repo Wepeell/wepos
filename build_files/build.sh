c716c35ea48ff30241da7b4ae4f947544e80bc36
Wepeell <erik_ego@hotmail.com>
2025-06-02 17:18:46 +0200

    Remove Starship package in favour of Homebrew

--- build_files/build.sh
+++ build_files/build.sh
@@ -39,13 +39,13 @@ fi
 
 
 # Install Starship Shell Prompt
-echo "Installing Starship Shell Prompt..."
-if /ctx/install-starship.sh; then
-	echo "Starship Shell Prompt installed!"
-else
-	echo "install-starship.sh failed! Exiting."
-	exit 1
-fi
+# echo "Installing Starship Shell Prompt..."
+# if /ctx/install-starship.sh; then
+# 	echo "Starship Shell Prompt installed!"
+# else
+# 	echo "install-starship.sh failed! Exiting."
+# 	exit 1
+# fi
 
 
 
