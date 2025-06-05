2676094e49ba751f463df28499f02b4b8bae6ff9
Wepeell <erik_ego@hotmail.com>
2025-06-05 05:47:45 +0200

    Copy just files to base image

--- build_files/add-ujust-cmds.sh
+++ build_files/add-ujust-cmds.sh
@@ -2,6 +2,9 @@
 
 set -ouex pipefail
 
-# Add custom ujust commands
+# Copy just files to base image
+cp /ctx/system_files/usr/share/bazzite-wepeell/just/*.just /usr/share/bazzite-wepeell/just
+
+# Enable custom ujust commands
 echo "import \"/usr/share/bazzite-wepeell/just/nerd-fonts.just\"" >> /usr/share/ublue-os/justfile
 echo "import \"/usr/share/bazzite-wepeell/just/starship.just\"" >> /usr/share/ublue-os/justfile
