d118284ec49149fa2cea4d868f2c47f72bc92770
Wepeell <erik_ego@hotmail.com>
2025-09-15 04:40:52 +0200

    Add comments on how to enable RPM Fusion repos (#92)

--- build_files/install-fedora-pkgs.sh
+++ build_files/install-fedora-pkgs.sh
@@ -2,6 +2,11 @@
 
 set -ouex pipefail
 
+# Enable RPM Fusion Repository
+# dnf5 -y config-manager setopt "rpmfusion-nonfree".enabled=true
+# dnf5 -y config-manager setopt "rpmfusion-free".enabled=true
+# dnf5 -y config-manager setopt "*rpmfusion*".enabled=true
+
 # Install Fedora packages
 dnf5 -y install \
 	plasma-wallpapers-dynamic \
