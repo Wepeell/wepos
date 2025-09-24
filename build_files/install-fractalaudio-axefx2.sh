261f0b12aff9dec25c0230e52a69073bc44b13a8
Wepeell <erik_ego@hotmail.com>
2025-09-24 02:50:08 +0200

    Add install script for Axe-Fx 2 USB firmware binary

---
+++ build_files/install-fractalaudio-axefx2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+### Variables
+
+# Axe-Fx 2 USB firmware
+FIRMWARE_SOURCE="/ctx/repo_files/axefx2-usb-firmware"
+FIRMWARE_TARGET="/usr/share/usb/FractalAudio/axefx2"
+FIRMWARE_BIN="axefx2load.hex"
+
+### Install required packages
+dnf5 -y install \
+	fxload
+
+### Install USB firmware binary
+
+# Create directory
+mkdir -p "$FIRMWARE_TARGET"
+
+# Set permissions for both "FractalAudio" and "axefx2" directories
+chmod 755 "$FIRMWARE_TARGET" \
+	"${FIRMWARE_TARGET%/*}"
+
+# Copy binary
+cp "$FIRMWARE_SOURCE/$FIRMWARE_BIN" "$FIRMWARE_TARGET/$FIRMWARE_BIN"
+
+# Set binary permissions
+chmod 644 "$FIRMWARE_TARGET/$FIRMWARE_BIN"
