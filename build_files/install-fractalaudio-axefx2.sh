108ef130a680b4f54d59dbad47be3805e1feb006
Wepeell <erik_ego@hotmail.com>
2025-09-24 04:27:47 +0200

    Add USB firmware and udev rule for Fractal Audio Axe-Fx 2 (#98)
    * Add Axe-Fx 2 USB firmware to repo
    
    * Add install script for Axe-Fx 2 USB firmware binary
    
    * Enable Axe-Fx 2 USB firmware install script
    
    * Add udev rule for Axe-Fx 2 USB firmware
    
    * Add info about Axe-Edit, Fractal-Bot and Wine

---
+++ build_files/install-fractalaudio-axefx2.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+### Variables
+
+# Axe-Fx 2 USB firmware
+FIRMWARE_SOURCE="/ctx/repo_files/axefx2-usb-firmware"
+FIRMWARE_TARGET="/usr/share/usb/FractalAudio/axefx2"
+FIRMWARE_BIN="axefx2load.hex"
+
+# udev rules
+UDEV_DIR="/etc/udev/rules.d"
+UDEV_FILE="99-fractalaudio.rules"
+# FXLOAD_PATH="$(which fxload)"
+FXLOAD_PATH="/usr/bin/fxload"
+
+### Install required packages
+# dnf5 -y install \
+#	fxload
+
+### Install USB firmware binary
+
+# Create directory
+mkdir -p "$FIRMWARE_TARGET"
+
+# Set permissions for both "FractalAudio" and "axefx2" directories
+chmod 755 "$FIRMWARE_TARGET" \
+	"${FIRMWARE_TARGET%/*}"
+
+# Copy binary
+cp "$FIRMWARE_SOURCE/$FIRMWARE_BIN" "$FIRMWARE_TARGET/$FIRMWARE_BIN"
+
+# Set binary permissions
+chmod 644 "$FIRMWARE_TARGET/$FIRMWARE_BIN"
+
+### Install udev rule
+
+# Create directory
+mkdir -p "$UDEV_DIR"
+
+# Create udev rules file
+cat <<EOF > "$UDEV_DIR/$UDEV_FILE"
+### fractalaudio.rules - udev rules for uploading USB firmware to Fractal Audio Systems devices
+
+# Fractal Audio Systems Axe-FX II
+ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="2466", ATTR{idProduct}=="0003", RUN+="'$FXLOAD_PATH' -t fx2lp -I '$FIRMWARE_TARGET/$FIRMWARE_BIN' -D \$env{DEVNAME}"
+EOF
+
+### Important notes
+# When installing Axe-Edit for Axe-Fx 2 use Wine version "lutris-6.21-6"
+# There is a regression in Wine around version 7.2
+# https://bugs.winehq.org/show_bug.cgi?id=56961
+
+# When installing Fractal-Bot use version "Fractal-Bot-Win-v3p00p03.exe"
+# Regressions in newer versions of Fractal-Bot
+# Links: https://forum.fractalaudio.com/threads/axe-fx-ii-users-on-linux-use-fractal-bot-v3p00p03.203669/
