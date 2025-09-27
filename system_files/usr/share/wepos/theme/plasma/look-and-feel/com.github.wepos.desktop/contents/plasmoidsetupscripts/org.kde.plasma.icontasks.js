d286939389fb54fc9be6d369dda0c02dc295c45d
Wepeell <erik_ego@hotmail.com>
2025-09-27 04:06:57 +0200

    Update Icons-Only Task Manager with default settings

--- system_files/usr/share/wepos/theme/plasma/look-and-feel/com.github.wepos.desktop/contents/plasmoidsetupscripts/org.kde.plasma.icontasks.js
+++ system_files/usr/share/wepos/theme/plasma/look-and-feel/com.github.wepos.desktop/contents/plasmoidsetupscripts/org.kde.plasma.icontasks.js
@@ -1,8 +1,25 @@
-// Set default pinned apps
+// Set config group
 applet.currentConfigGroup = ["General"]
+
+// Clicking grouped task: Shows small window previews
+applet.writeConfig("groupedTaskVisualization", "1")
+
+// General: Mute task when clicking indicator
+applet.writeConfig("interactiveMute", "false")
+
+// Set default pinned apps
 applet.writeConfig("launchers", [
     "applications:org.gnome.Ptyxis.desktop",
     "applications:org.kde.dolphin.desktop",
+    "applications:org.keepassxc.KeePassXC.desktop",
+    "applications:org.fooyin.fooyin.desktop",
+    "applications:steam.desktop",
+    "applications:com.discordapp.Discord.desktop",
     "preferred://browser"
 ]);
+
+// General: Show media and volume controls in tooltip
+applet.writeConfig("tooltipControls", "false")
+
+// Reload config
 applet.reloadConfig()
