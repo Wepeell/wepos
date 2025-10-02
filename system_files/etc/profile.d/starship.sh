da427f0248a66ff9c3b190c16ba61b9ea1419754
Wepeell <erik_ego@hotmail.com>
2025-10-02 04:59:43 +0200

    Set environmental variable path for Starship config

---
+++ system_files/etc/profile.d/starship.sh
@@ -0,0 +1,7 @@
+# This sets the config path for Starship prompt system-wide
+export STARSHIP_CONFIG=/etc/starship.toml
+
+# To override this per user, unset the environmental variable in $HOME/.bashrc like this:
+# unset STARSHIP_CONFIG
+
+# Starship will then fall back to using the default path $HOME/.config/starship.toml
