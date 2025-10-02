6e09933a5f41c7ff36dca3b33c9e35fb1a2655a5
Wepeell <erik_ego@hotmail.com>
2025-10-02 04:06:41 +0200

    Refactor COPR packages script (#121)

--- build_files/install-copr-pkgs.sh
+++ build_files/install-copr-pkgs.sh
@@ -2,19 +2,31 @@
 
 set -ouex pipefail
 
-### CoolerControl
+# Repos array
+repos=(
+	codifryed/CoolerControl
+)
 
-# Enable COPR repo
-dnf5 -y copr enable codifryed/CoolerControl
+# Packages array
+packages=(
+	coolercontrold
+	coolercontrol-liqctld
+	coolercontrol
+)
+
+# Enable COPR repos
+for repo in "${repos[@]}"; do
+	dnf5 -y copr enable "$repo"
+done
 
 # Install COPR packages
-dnf5 -y install \
-	coolercontrold \
-	coolercontrol-liqctld \
-	coolercontrol
+dnf5 -y install "${packages[@]}"
 
+# Disable COPR repos
+for repo in "${repos[@]}"; do
+	dnf5 -y copr disable "$repo"
+done
+
+# CoolerControl
 # Enable daemon
 systemctl enable coolercontrold
-
-# Disable COPR repo
-dnf5 -y copr disable codifryed/CoolerControl
