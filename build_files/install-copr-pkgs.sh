77c15924d16857d66f0c0d702d6eed1615068143
Wepeell <erik_ego@hotmail.com>
2025-10-20 14:25:53 +0200

    Enable Starship shell prompt through /etc/bashrc

--- build_files/install-copr-pkgs.sh
+++ build_files/install-copr-pkgs.sh
@@ -36,3 +36,11 @@ done
 # CoolerControl
 # Enable daemon
 systemctl enable coolercontrold
+
+# Starship
+# Insert into bashrc without expanding $(...)
+cat <<'EOF' >> "/etc/bashrc"
+
+# Enable Starship shell prompt
+eval "$(starship init bash)"
+EOF
