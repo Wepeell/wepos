72335060b0f6570160622efb1153b444c8a49fcf
Wepeell <4354676+Wepeell@users.noreply.github.com>
2025-11-02 05:53:33 +0100

    Disable Terra repo and pin plasma-wallpaper-dynamic (#155)
    * Pin version for plasma-wallpapers-dynamic
    
    * Disable Terra repo

--- build_files/build.sh
+++ build_files/build.sh
@@ -45,6 +45,9 @@ function run_script() {
 # gpg: Fatal: can't create directory '/root/.gnupg': No such file or directory
 mkdir -p /var/roothome
 
+# Disable Terra repo
+dnf5 -y config-manager setopt "terra*".enabled=false
+
 # Install Fedora packages
 run_script "/ctx/install-fedora-pkgs.sh"
 
