3504d638a3de0fb85eb9c616ba5a11136de91ff4
Wepeell <4354676+Wepeell@users.noreply.github.com>
2025-11-06 22:25:53 +0100

    Refactor fcp-support install script (#168)
    * Build script: Update fcp-support install script
    
    * Build script: Refactor install-fcp-support.sh

--- build_files/install-fcp-support.sh
+++ build_files/install-fcp-support.sh
@@ -5,57 +5,48 @@ set -ouex pipefail
 # Repo directory
 repo_dir="/tmp/fcp-support"
 
-# Install directories
-bin_dir="/usr/bin"
-systemd_dir="/usr/lib/systemd/system"
-udev_dir="/usr/lib/udev/rules.d"
-data_dir="/usr/share/fcp-server"
+# Directory prefix
+prefix_dir="/usr"
 
-# Install required packages for building
-dnf5 -y install \
-	make \
-	gcc \
-	alsa-lib-devel \
-	systemd-devel \
-	openssl-devel \
-	zlib-devel \
-	json-c-devel \
-	pkgconfig
+# Packages array
+packages=(
+	# make
+	# gcc
+	alsa-lib-devel
+	systemd-devel
+	# openssl-devel
+	# zlib-devel
+	json-c-devel
+	# pkgconfig
+)
+
+# Check if base image packages are being replaced
+if dnf5 install --setopt=tsflags=test -y "${packages[@]}" | grep -E '^(Upgrading|Downgrading):'; then
+	echo "Detected package replacements. Aborting build."
+	exit 1
+fi
+
+# Install required packages for building from array
+dnf5 -y install "${packages[@]}"
 
 # Clone repo
-git clone https://github.com/Wepeell/fcp-support.git "$repo_dir"
+git clone https://github.com/Wepeell/fcp-support.git "${repo_dir}"
 
 # Build
-make -C "$repo_dir"
-
-# Create install directories
-mkdir -p "$bin_dir"
-mkdir -p "$systemd_dir"
-mkdir -p "$udev_dir"
-mkdir -p "$data_dir"
+make \
+	-C "${repo_dir}" \
+	PREFIX=${prefix_dir}
 
 # Install
-cp "${repo_dir}/fcp-server" "$bin_dir"
-cp "${repo_dir}/fcp-tool" "$bin_dir"
-cp "${repo_dir}/systemd/fcp-server@.service" "$systemd_dir"
-cp "${repo_dir}/udev/99-fcp.rules" "$udev_dir"
-cp "${repo_dir}/data"/fcp-alsa-map-*.json "$data_dir"
-
-# Set permissions
-chmod 755 "${bin_dir}/fcp-server"
-chmod 755 "${bin_dir}/fcp-tool"
-chmod 644 "${systemd_dir}/fcp-server@.service"
-chmod 644 "${udev_dir}/99-fcp.rules"
-chmod 644 "${data_dir}"/fcp-alsa-map-*.json
+make install \
+	-C "${repo_dir}" \
+	PREFIX=${prefix_dir}
 
 # Cleanup repo
-rm -rf "$repo_dir"
+rm -rf "${repo_dir}"
 
-# Uninstall packages for building
-dnf5 -y remove \
-	alsa-lib-devel \
-	systemd-devel \
-	json-c-devel
+# Uninstall packages for building from array
+dnf5 -y remove "${packages[@]}"
 
 # Audio group membership is required to use fcp-tool
 # sudo usermod -a -G audio $USER
