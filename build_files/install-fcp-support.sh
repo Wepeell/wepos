4e03ad047e2d285c8029398289f7d836daea9b8d
Wepeell <4354676+Wepeell@users.noreply.github.com>
2025-11-06 22:09:28 +0100

    Build script: Refactor install-fcp-support.sh

--- build_files/install-fcp-support.sh
+++ build_files/install-fcp-support.sh
@@ -5,38 +5,48 @@ set -ouex pipefail
 # Repo directory
 repo_dir="/tmp/fcp-support"
 
-# Install required packages for building
-dnf5 -y install \
-	make \
-	gcc \
-	alsa-lib-devel \
-	systemd-devel \
-	openssl-devel \
-	zlib-devel \
-	json-c-devel \
-	pkgconfig
+# Directory prefix
+prefix_dir="/usr"
+
+# Packages array
+packages=(
+	# make
+	# gcc
+	alsa-lib-devel
+	systemd-devel
+	# openssl-devel
+	# zlib-devel
+	json-c-devel
+	# pkgconfig
+)
+
+# Check if base image packages are being replaced
+if dnf5 install --setopt=tsflags=test -y "${packages[@]}" | grep -E '^(Upgrading|Downgrading):'; then
+	echo "Detected package replacements. Aborting build."
+	exit 1
+fi
+
+# Install required packages for building from array
+dnf5 -y install "${packages[@]}"
 
 # Clone repo
-git clone https://github.com/Wepeell/fcp-support.git "$repo_dir"
+git clone https://github.com/Wepeell/fcp-support.git "${repo_dir}"
 
 # Build
 make \
-	-C "$repo_dir" \
-	PREFIX=/usr
+	-C "${repo_dir}" \
+	PREFIX=${prefix_dir}
 
 # Install
 make install \
-	-C "$repo_dir" \
-	PREFIX=/usr
+	-C "${repo_dir}" \
+	PREFIX=${prefix_dir}
 
 # Cleanup repo
-rm -rf "$repo_dir"
+rm -rf "${repo_dir}"
 
-# Uninstall packages for building
-dnf5 -y remove \
-	alsa-lib-devel \
-	systemd-devel \
-	json-c-devel
+# Uninstall packages for building from array
+dnf5 -y remove "${packages[@]}"
 
 # Audio group membership is required to use fcp-tool
 # sudo usermod -a -G audio $USER
