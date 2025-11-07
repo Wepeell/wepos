92905a0833d6f88fbf767d1c93d6247f3ffd3a9a
Wepeell <4354676+Wepeell@users.noreply.github.com>
2025-11-07 08:31:29 +0100

    Build files: Add fcp-support rpm package

--- build_files/install-focusrite-scarlett.sh
+++ build_files/install-focusrite-scarlett.sh
@@ -4,6 +4,9 @@ set -ouex pipefail
 
 ### Target directories
 
+# Linux FCP Support Tools
+fcp_dir="/ctx/repo_files/fcp-support"
+
 # Scarlett4 Firmware
 firmware_dir="/ctx/repo_files/scarlett4-firmware"
 
@@ -12,5 +15,6 @@ gui_dir="/ctx/repo_files/alsa-scarlett-gui"
 
 # Install packages
 dnf5 -y install \
+	"${fcp_dir}"/* \
 	"${firmware_dir}"/* \
 	"${gui_dir}"/*
