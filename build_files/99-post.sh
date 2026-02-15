f30d228fbe48c3f4ac9f709d96ff823e9f7b74e1
Wepeell <4354676+Wepeell@users.noreply.github.com>
2026-02-16 00:40:00 +0100

    Build files: Add Mullvad VPN (#240)

--- build_files/99-post.sh
+++ build_files/99-post.sh
@@ -11,6 +11,10 @@ cp -r /ctx/system_files/* /
 # Remove autostart files
 rm -f /etc/skel/.config/autostart/steam.desktop
 
+# Copy /opt files into an immutable part of the image
+# These files needs to be copied or symlinked back into /var/opt on the running system to be used
+cp -r /var/opt /usr/share/wepos/opt
+
 ### Add kargs
 # cat <<'EOF' > "/usr/lib/bootc/kargs.d/99-amd.toml"
 # kargs = ["amdgpu.ppfeaturemask=0xffffffff"]
