a8af6652884e6c8f795b3f2d746eeef95956a00e
Wepeell <4354676+Wepeell@users.noreply.github.com>
2026-02-17 00:59:37 +0100

    Build files: Enable Mullvad VPN repository for local layering

--- build_files/99-post.sh
+++ build_files/99-post.sh
@@ -15,6 +15,10 @@ rm -f /etc/skel/.config/autostart/steam.desktop
 # These files needs to be copied or symlinked back into /var/opt on the running system to be used
 cp -r /var/opt/. /usr/share/wepos/opt/
 
+# Enable Mullvad VPN repo for local layering
+# rpm-ostree install mullvad-vpn
+dnf5 -y config-manager addrepo --from-repofile="https://repository.mullvad.net/rpm/stable/mullvad.repo"
+
 ### Add kargs
 # cat <<'EOF' > "/usr/lib/bootc/kargs.d/99-amd.toml"
 # kargs = ["amdgpu.ppfeaturemask=0xffffffff"]
