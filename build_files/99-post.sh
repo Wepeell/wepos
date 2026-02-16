c6079dd2b1636253ed423192e3ed8e3a085b544b
Wepeell <4354676+Wepeell@users.noreply.github.com>
2026-02-16 23:37:49 +0100

    Build files: Update cp /var/opt command

--- build_files/99-post.sh
+++ build_files/99-post.sh
@@ -11,9 +11,9 @@ cp -r /ctx/system_files/* /
 # Remove autostart files
 rm -f /etc/skel/.config/autostart/steam.desktop
 
-# Copy /opt files into an immutable part of the image
+# Copy /opt content into an immutable part of the image
 # These files needs to be copied or symlinked back into /var/opt on the running system to be used
-cp -r /var/opt /usr/share/wepos/opt
+cp -r /var/opt/. /usr/share/wepos/opt/
 
 ### Add kargs
 # cat <<'EOF' > "/usr/lib/bootc/kargs.d/99-amd.toml"
