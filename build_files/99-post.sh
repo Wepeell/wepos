c09e6a7c0d98faa54ced2be8ded9ee31491b45e5
Wepeell <4354676+Wepeell@users.noreply.github.com>
2026-02-17 01:26:14 +0100

    Use local layering for Mullvad VPN (#242)
    * Build files: Enable Mullvad VPN repository for local layering
    
    * Build files: Do not create /var/opt
    
    * Build files: Do not copy /var/opt
    
    * Build files: remove rpm-install docs
    
    * Build files: Remove Mullvad VPN install script

--- build_files/99-post.sh
+++ build_files/99-post.sh
@@ -11,9 +11,8 @@ cp -r /ctx/system_files/* /
 # Remove autostart files
 rm -f /etc/skel/.config/autostart/steam.desktop
 
-# Copy /opt content into an immutable part of the image
-# These files needs to be copied or symlinked back into /var/opt on the running system to be used
-cp -r /var/opt/. /usr/share/wepos/opt/
+# Enable Mullvad VPN repo for local layering
+dnf5 -y config-manager addrepo --from-repofile="https://repository.mullvad.net/rpm/stable/mullvad.repo"
 
 ### Add kargs
 # cat <<'EOF' > "/usr/lib/bootc/kargs.d/99-amd.toml"
