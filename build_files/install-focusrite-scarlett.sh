f56e4696dfd0a8cc092abf0113288c1b95eb1ab9
Wepeell <4354676+Wepeell@users.noreply.github.com>
2026-08-16 02:28:01 +0200

    cleanup comments

--- build_files/install-focusrite-scarlett.sh
+++ build_files/install-focusrite-scarlett.sh
@@ -2,13 +2,12 @@
 
 set -ouex pipefail
 
-### Packages array
-# Target directories as elements
+# Packages to install
 packages=(
     /ctx/repo_files/fcp-support/*
     /ctx/repo_files/scarlett4-firmware/*
     /ctx/repo_files/alsa-scarlett-gui/*
 )
 
-### Install packages
+# Install packages
 dnf5 -y install "${packages[@]}"
