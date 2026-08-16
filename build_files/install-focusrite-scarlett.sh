bf4c91b407c18d5ecdaaf3feeafeca9e0f8269ef
Wepeell <4354676+Wepeell@users.noreply.github.com>
2026-08-16 02:28:54 +0200

    add package replacment check to focusrite packages

--- build_files/install-focusrite-scarlett.sh
+++ build_files/install-focusrite-scarlett.sh
@@ -9,5 +9,14 @@ packages=(
     /ctx/repo_files/alsa-scarlett-gui/*
 )
 
+# Check if base image packages are being replaced with a dry run
+dnf5 --setopt=tsflags=test -y install "${packages[@]}" 2>&1 | tee /tmp/dryrun.log
+
+# Check log for upgrading and downgrading
+if grep -qE '^(Upgrading|Downgrading):' /tmp/dryrun.log; then
+    echo "::notice::Detected package replacements. Aborting build."
+    exit 1
+fi
+
 # Install packages
 dnf5 -y install "${packages[@]}"
