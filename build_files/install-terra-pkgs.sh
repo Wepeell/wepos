e848cd3931331ba7750ec4d505fe59968c5a00e9
Wepeell <4354676+Wepeell@users.noreply.github.com>
2026-08-16 17:58:09 +0200

    add terra script template

---
+++ build_files/install-terra-pkgs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+# Packages to install
+packages=(
+    starship
+)
+
+# Enable repos
+# dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release terra-gpg-keys
+dnf5 -y config-manager setopt "terra".enabled=true
+
+# Check if base image packages are being replaced with a dry run
+dnf5 --setopt=tsflags=test -y install "${packages[@]}" 2>&1 | tee /tmp/dryrun.log
+
+# Check log for upgrading and downgrading
+if grep -qE '^(Upgrading|Downgrading):' /tmp/dryrun.log; then
+    echo "::notice::Detected package replacements. Aborting build."
+    exit 1
+fi
+
+# Install packages
+dnf5 -y install "${packages[@]}"
+
+# Disable repos
+dnf5 -y config-manager setopt "terra*".enabled=false
