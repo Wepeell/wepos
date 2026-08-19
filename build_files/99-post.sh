7ba91bbf4741f09609a7f6f67a28deaa993aa4cb
Wepeell <4354676+Wepeell@users.noreply.github.com>
2026-08-19 16:48:48 +0200

    disable xwaylandvideobridge autostart (#344)

--- build_files/99-post.sh
+++ build_files/99-post.sh
@@ -11,6 +11,11 @@ cp -rv "/ctx/system_files"/. /
 # Remove autostart files
 rm -f /etc/skel/.config/autostart/steam.desktop
 
+# Disable autostart
+tee -a "/etc/xdg/autostart/org.kde.xwaylandvideobridge.desktop" <<'EOF'
+Hidden=true
+EOF
+
 ### Add kargs
 # tee "/usr/lib/bootc/kargs.d/99-amd.toml" <<'EOF'
 # kargs = ["amdgpu.ppfeaturemask=0xffffffff"]
