86ec493861ab9d038788af0a513a3c0a16a7dc21
Wepeell <4354676+Wepeell@users.noreply.github.com>
2026-08-31 23:33:20 +0200

    clean up directories to make linter stop complaining (#357)

--- build_files/99-post.sh
+++ build_files/99-post.sh
@@ -16,6 +16,11 @@ tee -a "/etc/xdg/autostart/org.kde.xwaylandvideobridge.desktop" <<'EOF'
 Hidden=true
 EOF
 
+# Clean up directories to make linter stop complaining
+rm -rf /run/dnf
+rm -rf /run/fcp-server
+rm -rf /var/lib/dnf/repos
+
 ### Add kargs
 # tee "/usr/lib/bootc/kargs.d/99-amd.toml" <<'EOF'
 # kargs = ["amdgpu.ppfeaturemask=0xffffffff"]
